164e70e6b7c66dd96ea5c635e6b86a070a9033e2
Adem <adematicis41@gmail.com>
2022-04-04 10:53:45 +0300

    Add view binding dependency

--- app/src/main/java/com/adematici/turkcellsinav2/MainActivity.kt
+++ app/src/main/java/com/adematici/turkcellsinav2/MainActivity.kt
@@ -2,10 +2,17 @@ package com.adematici.turkcellsinav2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.adematici.turkcellsinav2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
     }
 }
